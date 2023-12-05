632b1cf4d456798873bc21f63a37b5ed08436e32
trolleybusartem <112125354+trolleybusartem@users.noreply.github.com>
2023-12-05 21:56:58 +0300

    работаем

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
 while True:
-  text = input()
-  text = text.replace("З", "Z")
-  text = text.replace("з", "Z")
-  text = text.replace("В", "V")
-  text = text.replace("в", "V")
-  text = text.replace("О", "O")
-  text = text.replace("о", "O")
-  print(text)
+    print(input().replace("З", "Z").replace("з", "Z").replace("В", "V").replace("в", "V").replace("о", "O"))
